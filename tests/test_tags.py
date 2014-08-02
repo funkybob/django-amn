302a1f6820dec77a5ef44e9f1a30bfc76c11d240
funkybob <curtis@tinbrain.net>
2014-08-02 13:20:26 +1000

    Add more tests

--- tests/test_tags.py
+++ tests/test_tags.py
@@ -1,5 +1,4 @@
 
-#from unittest import TestCase
 from django.test import TestCase
 
 from django.test.utils import setup_test_template_loader, override_settings
@@ -8,7 +7,7 @@ from django.template.loader import get_template
 
 TEMPLATES = {
     'basetag': '''{% load damn %}{% assets %}''',
-    'test2': '''
+    'test_one': '''
 <!doctype html>{% load damn %}
 <html>
 <head>
@@ -19,6 +18,31 @@ TEMPLATES = {
 </body>
 </html>
 ''',
+    'test_two': '''
+<!doctype html>{% load damn %}
+<html>
+<head>
+{% assets %}
+</head>
+<body>
+{% asset 'js/jquery.js' %}
+{% asset 'js/knockout.js' %}
+</body>
+</html>
+''',
+    'test_three': '''
+<!doctype html>{% load damn %}
+<html>
+<head>
+{% assets %}
+</head>
+<body>
+{% asset 'js/knockout.js' 'js/jquery.js' %}
+{% asset 'js/jquery.js' %}
+</body>
+</html>
+''',
+
 }
 
 DAMN_PROCESSORS = {
@@ -27,25 +51,53 @@ DAMN_PROCESSORS = {
     },
 }
 
+DEFAULT_SETTINGS = {
+    'STATIC_URL': '/static/',
+}
+
 class TagTests(TestCase):
 
-    def setUp(self):
+    @classmethod
+    def setUpClass(self):
         setup_test_template_loader(TEMPLATES)
 
     @override_settings(
         DAMN_PROCESSORS=DAMN_PROCESSORS,
-        STATIC_URL = '/',
+        **DEFAULT_SETTINGS
     )
     def test_simple(self):
         t = get_template('basetag')
-        t.render()
+        t.render(Context())
 
     @override_settings(
         DAMN_PROCESSORS=DAMN_PROCESSORS,
-        STATIC_URL = '/',
+        **DEFAULT_SETTINGS
     )
     def test_one(self):
-        t = get_template('test2')
+        t = get_template('test_one')
         o = t.render(Context())
         self.assertTrue('<script src="/static/js/jquery.js"></script>' in o)
 
+    @override_settings(
+        DAMN_PROCESSORS=DAMN_PROCESSORS,
+        **DEFAULT_SETTINGS
+    )
+    def test_two(self):
+        t = get_template('test_two')
+        o = t.render(Context())
+        self.assertTrue('<script src="/static/js/jquery.js"></script>' in o)
+        self.assertTrue('<script src="/static/js/knockout.js"></script>' in o)
+
+    @override_settings(
+        DAMN_PROCESSORS=DAMN_PROCESSORS,
+        **DEFAULT_SETTINGS
+    )
+    def test_three(self):
+        t = get_template('test_two')
+        o = t.render(Context())
+        print o
+        self.assertTrue('<script src="/static/js/jquery.js"></script>' in o)
+        self.assertTrue('<script src="/static/js/knockout.js"></script>' in o)
+        self.assertTrue(
+            o.index('src="/static/js/jquery.js"') < o.index('src="/static/js/knockout.js"')
+        )
