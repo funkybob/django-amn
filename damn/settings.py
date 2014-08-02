b3f53c9673e8d044516d0c38403b7e5758ec1840
funkybob <curtis@tinbrain.net>
2014-08-02 11:20:53 +1000

    Add more settings

--- damn/settings.py
+++ damn/settings.py
@@ -3,4 +3,18 @@ from django.conf import settings
 
 PROCESSORS = getattr(settings, 'DAMN_PROCESSORS', {})
 
+# File extensin -> mode name
 MODE_MAP = getattr(settings, 'DAMN_MODE_MAP', {})
+
+# Map of maps of default mode aliases
+#   {
+#       'js': {
+#           'jquery': 'js/libs/jquery-1.11.min.js',
+#       },
+#       'css': {
+#           'bootstrap': 'css/vendors/bootstrap-3.0.min.js',
+#       },
+#   }
+DEPS = getattr(settings, 'DAMN_DEPENDS', {})
+
+MODE_ORDER = getattr(settings, 'DAMN_MODE_ORDER', ['css', 'js', ])
