13d6f0eb7adb8e6a8e12142fdf750b0ea074f6cc
funkybob <curtis@tinbrain.net>
2014-08-03 13:27:47 +1000

    Add a setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+
+
+setup(
+    name='django-amn',
+    version='0.1.1',
+    description='Django asset dependency management.',
+    author='Curtis Maloney',
+    author_email='curtis@tinbrain.net',
+    keywords=['django', 'assets',],
+    packages=[
+        'damn',
+        'damn.templatetags',
+    ],
+    zip_safe=False,
+)
+
